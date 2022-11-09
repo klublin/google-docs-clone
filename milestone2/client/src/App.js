ceca2431f5926702a413a9c0edd413f6e0f99193
kevwei1 <89945128+kevwei1@users.noreply.github.com>
2022-11-09 16:44:27 -0500

    milestone2 init

---
+++ milestone2/client/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import TextEditor from './TextEditor.js'
+import FrontPage from './FrontPage.js'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router> 
+      <Routes>
+        <Route path='/' element={<FrontPage/>}/>
+        <Route path='/api/:id' element={<TextEditor/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
---
+++ milestone2/client/src/TextEditor.js
@@ -0,0 +1,60 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Quill from "quill"
+import "quill/dist/quill.snow.css"
+import * as Y from 'yjs'
+import { QuillBinding } from 'y-quill'
+
+//THINK WHY NOT CHANGE B/C IT IS NOT CORRECT TYPE
+export default function TextEditor() {
+    let [ doc, setDoc ] = useState();
+
+    let { id } = useParams();
+    const TOOLBAR_OPTIONS = [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+    ]
+    const wrapperRef = useCallback(wrapper =>{
+        if(wrapper == null)return;
+
+        wrapper.innerHTML = "";
+        const editor = document.createElement("div");
+        wrapper.append(editor);
+        
+        const ydoc = new Y.Doc()
+        setDoc(ydoc);
+        const ytext = ydoc.getText('quill') 
+        const d = new Quill(editor, {theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS}});
+
+        new QuillBinding(ytext, d);
+        ydoc.on('update', update => {
+            console.log("a");
+            fetch(`http://localhost:3001/api/op/${id}`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(Array.from(update))
+            });
+        })
+    },[])
+    useEffect(()=>{
+        if(doc === undefined ){
+            return;
+        }
+        const events = new EventSource(`http://localhost:3001/api/connect/${id}`)
+        events.addEventListener('sync', (event) =>{
+            let obj = JSON.parse(event.data);
+            obj = Uint8Array.from(obj);
+            Y.applyUpdate(doc, obj);
+        })
+        events.addEventListener('update', (event) =>{
+            let obj = JSON.parse(event.data);
+            obj = Uint8Array.from(obj);  
+            Y.applyUpdate(doc, obj);
+        })
+    },[doc])
+    return <div id = "container" ref={wrapperRef}></div>
+}
