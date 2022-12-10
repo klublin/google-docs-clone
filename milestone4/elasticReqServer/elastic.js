634163cae343cc6492b1ee74b79e9741c166baf0
Kevin <seabook02@gmail.com>
2022-12-10 01:09:15 -0500

    deployment details

--- milestone4/elasticReqServer/elastic.js
+++ milestone4/elasticReqServer/elastic.js
@@ -3,11 +3,11 @@ const memjs = require('memjs')
 
 const client = new Client({
     cloud: {
-        id: milestone4:dXMtZWFzdDQuZ2NwLmVsYXN0aWMtY2xvdWQuY29tJDM3YTg1MzIxMzQ0ZDQ4NGY5MmU0ODY2OGU4ZTFiYmZhJDc1N2VmNDQ2ZmUzZjRhYmM5NGNkOGYzYWM5MTcwNjZk
+        id: "milestone4:dXMtZWFzdDQuZ2NwLmVsYXN0aWMtY2xvdWQuY29tJDM3YTg1MzIxMzQ0ZDQ4NGY5MmU0ODY2OGU4ZTFiYmZhJDc1N2VmNDQ2ZmUzZjRhYmM5NGNkOGYzYWM5MTcwNjZk"
     }
     auth: {
-        username: elastic,
-        password: pZQFEceYTwtCJNnbQgOOdWsE
+        username: "elastic",
+        password: "pZQFEceYTwtCJNnbQgOOdWsE"
     }
 })
 
