cb24a08e740b8c59068230c96b1c275400b24f26
klublin <71909601+klublin@users.noreply.github.com>
2022-12-11 15:45:15 -0500

    change ip

--- milestone4/opServer/db/elasticClient.js
+++ milestone4/opServer/db/elasticClient.js
@@ -1,6 +1,6 @@
 const { Client } = require('@elastic/elasticsearch') 
 
-const client = new Client({ node: 'http://209.151.155.111:9200'})
+const client = new Client({ node: 'http://209.94.56.105:9200'})
 client.info().then(response => console.log("Connected to ES cloud!")).catch(error => console.log(error));
 
 module.exports = client;
