f1223691b604831a21f0de12a3c46809b9aa5e93
klublin <71909601+klublin@users.noreply.github.com>
2022-12-11 15:45:26 -0500

    change connect message

--- milestone4/opServer/db/elasticClient.js
+++ milestone4/opServer/db/elasticClient.js
@@ -1,6 +1,6 @@
 const { Client } = require('@elastic/elasticsearch') 
 
 const client = new Client({ node: 'http://209.94.56.105:9200'})
-client.info().then(response => console.log("Connected to ES cloud!")).catch(error => console.log(error));
+client.info().then(response => console.log("Connected to ES!")).catch(error => console.log(error));
 
 module.exports = client;
