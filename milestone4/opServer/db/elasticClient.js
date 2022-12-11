4d89fb68f51e789af711ba49a89a26eacc5164e8
klublin <71909601+klublin@users.noreply.github.com>
2022-12-10 23:51:46 -0500

    cloud sucks

--- milestone4/opServer/db/elasticClient.js
+++ milestone4/opServer/db/elasticClient.js
@@ -1,14 +1,6 @@
 const { Client } = require('@elastic/elasticsearch') 
 
-const client = new Client({
-    cloud: {
-        id: "milestone4:dXMtZWFzdDQuZ2NwLmVsYXN0aWMtY2xvdWQuY29tJDM3YTg1MzIxMzQ0ZDQ4NGY5MmU0ODY2OGU4ZTFiYmZhJDc1N2VmNDQ2ZmUzZjRhYmM5NGNkOGYzYWM5MTcwNjZk"
-    },
-    auth: {
-        username: "elastic",
-        password: "pZQFEceYTwtCJNnbQgOOdWsE"
-    }
-})
+const client = new Client({ node: 'http://209.151.155.111:9200'})
 client.info().then(response => console.log("Connected to ES cloud!")).catch(error => console.log(error));
 
 module.exports = client;
