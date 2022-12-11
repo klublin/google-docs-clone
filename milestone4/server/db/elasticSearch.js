bfe280007a6de9f3a3d592fdb4363731488d1816
klublin <71909601+klublin@users.noreply.github.com>
2022-12-11 15:43:39 -0500

    ip change

--- milestone4/server/db/elasticSearch.js
+++ milestone4/server/db/elasticSearch.js
@@ -1,6 +1,6 @@
 const { Client } = require('@elastic/elasticsearch') 
 const client = new Client({
-    node: 'http://209.94.57.93:9200'
+    node: 'http://209.94.56.105:9200'
 })
 
 module.exports = client;
